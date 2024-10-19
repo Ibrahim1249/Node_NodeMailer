b421f8bfca5d7d26b67c30d9ab794c2c7935ace3
Ibrahim1249 <mominibrahim98.im85@gmail.com>
2024-10-19 12:40:37 +0530

    fixing bug

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { sendMailer, revertBackMail } = require("./mailer");
-const checkValidEmail = require("./middleware/verifymail");
+// const checkValidEmail = require("./middleware/verifymail");
 dotenv.config();
 
 const app = express()
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
 
-app.post("/mail",checkValidEmail ,(req,res)=>{
+app.post("/mail",(req,res)=>{
     const {name , email , message} = req.body;
     sendMailer(process.env.MY_USERNAME , process.env.MY_PASSWORD , name , email , message , res);
     revertBackMail(process.env.MY_USERNAME , process.env.MY_PASSWORD , name , email , message , res) 
